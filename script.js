bd80e4db4d089d57831e9160d465752fad8a5647
rogan-source <rogerdu754@gmail.com>
2023-05-13 16:59:51 +1000

    Testing

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ document.documentElement.setAttribute('data-scroll', window.scrollY);
 window.addEventListener('scroll', function() {
     document.documentElement.setAttribute('data-scroll', window.scrollY);
 });
+
+
+window.onload = (event) => {
+    const span = (text, index) => {
+        const node = document.createElement('span');
+        node.textContent = text;
+        node.style.setProperty('--index', index);
+        return node;
+    }
+    
+    const byLetter = text => [...text].map(span);
+    const splitTargets = document.querySelectorAll('[split]');
+    
+    splitTargets.forEach(function(node) {
+        let nodes = byLetter(node.innerText)
+    
+        for(var i = 0 ; i < nodes.length ; i++) {
+            nodes[i].style.setProperty('--total', nodes.length);
+        }
+
+
+        if (nodes)
+            node.firstChild.replaceWith(...nodes)
+    });
+    
+};
